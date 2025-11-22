bffbbd1d6623ec5d8614420ac4e68111f08e956d
Stoyan Mihaylov <stoyan.mihaylov950907@gmail.com>
2025-11-22 14:07:54 +0200

    - Add QueryableExtensions; - Add FootballLeagueDbContext;

---
+++ FootballLeague.Infrastructure/Extensions/QueryableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace FootballLeague.Infrastructure.Extensions
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> WithTracking<T>(
+            this IQueryable<T> query,
+            bool isTracked)
+            where T : class
+            => isTracked ? query : query.AsNoTracking();
+    }
+}
---
+++ FootballLeague.Infrastructure/FootballLeagueDbContext.cs
@@ -0,0 +1,24 @@
+﻿using FootballLeague.Domain.Entities;
+using FootballLeague.Infrastructure.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace FootballLeague.Infrastructure
+{
+    public class FootballLeagueDbContext : DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+
+        public DbSet<Match> Matches { get; set; }
+
+        public FootballLeagueDbContext(DbContextOptions<FootballLeagueDbContext> options)
+        : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new TeamConfiguration());
+            modelBuilder.ApplyConfiguration(new MatchConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
